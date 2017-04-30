72e3a95be2f405ab445c0ed0ae0ca81a7e9cf97d
Ella Jennings <erj503@gmail.com>
2017-04-30 12:37:14 -0700

    Ella - using conditional logic to generate pattern (challenge 14)

--- src/pfb/basics/FirstProgram.kt
+++ src/pfb/basics/FirstProgram.kt
@@ -19,7 +19,7 @@ class FirstProgram : Application() {
 
         for (row in 0..7) {
             for (col in 0..7) {
-                if(col == 0 || col == 2 || row == 0 || row == 2) {
+                if(row > 3 && col > 3) {
                     shades[row][col] = 0
                 } else {
                     shades[row][col] = 255
