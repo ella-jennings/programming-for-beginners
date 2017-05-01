b8a50be27f83761177b92c09f7c9062f1a6340e6
Ella Jennings <erj503@gmail.com>
2017-04-30 18:05:05 -0700

    Ella - Chapter 5: Challenge 21

--- src/pfb/basics/Arithmetic.kt
+++ src/pfb/basics/Arithmetic.kt
@@ -1,8 +1,11 @@
 package pfb.basics
 
 fun main(args: Array<String>) {
-    for(i in 1..10) {
-        val x = i*7
-        print("7*$i=$x\t")
+    for(s in 1..10) {
+        for (i in 1..10) {
+            val x = s * i
+            print("$s*$i=$x\t")
+        }
+        println()
     }
 }
