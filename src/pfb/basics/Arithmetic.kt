f25bf2d6011cc00e92c70d47df78b5d32faf73c3
Ella Jennings <erj503@gmail.com>
2017-05-01 16:17:00 -0700

    Ella - Chapter 6: initial setup

--- src/pfb/basics/Arithmetic.kt
+++ src/pfb/basics/Arithmetic.kt
@@ -1,11 +1,9 @@
 package pfb.basics
 
 fun main(args: Array<String>) {
-    for(s in 1..10) {
-        for (i in 1..10) {
-            val x = s * i
-            print("$s*$i=$x\t")
-        }
-        println()
+    var sum = 0
+    for (i in 1..10) {
+        sum = sum + i
     }
+    print(sum)
 }
