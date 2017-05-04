2fe95b9540b0b953e93d92cbd53b5471efdd1075
Ella Jennings <erj503@gmail.com>
2017-05-04 16:36:00 -0700

    Ella - Chapter 7: Challenge 33 (vowels)

--- src/pfb/basics/Chapter 7.kt
+++ src/pfb/basics/Chapter 7.kt
@@ -4,9 +4,9 @@ fun main(args: Array<String>) {
     val str = "How long is a piece of string?"
     var spaceCount = 0
     for (c in str) {
-        if (c == 'a' || c == 'e') {
+        if (c == 'a' || c == 'e' || c == 'i' || c == 'o' || c == 'u') {
             spaceCount = spaceCount + 1
         }
     }
-    println("Number of a and e characters: $spaceCount")
+    println("Number of vowels: $spaceCount")
 }
