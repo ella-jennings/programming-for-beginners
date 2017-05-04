9435e6f23e1b3e6b05c38b92c590870a3c81d5e0
Ella Jennings <erj503@gmail.com>
2017-05-04 16:34:34 -0700

    Ella - Chapter 7: Challenge 33 (a + e's)

--- src/pfb/basics/Chapter 7.kt
+++ src/pfb/basics/Chapter 7.kt
@@ -4,9 +4,9 @@ fun main(args: Array<String>) {
     val str = "How long is a piece of string?"
     var spaceCount = 0
     for (c in str) {
-        if (c == 'a') {
+        if (c == 'a' || c == 'e') {
             spaceCount = spaceCount + 1
         }
     }
-    println("Number of a's: $spaceCount")
+    println("Number of a and e characters: $spaceCount")
 }
