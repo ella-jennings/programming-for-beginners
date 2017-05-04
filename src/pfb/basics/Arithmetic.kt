d9fd3993f8ac41f9de2a15261b6653314caaeaf7
Ella Jennings <erj503@gmail.com>
2017-05-04 14:18:12 -0700

    Ella - Chapter 6: Challenge 28

--- src/pfb/basics/Arithmetic.kt
+++ src/pfb/basics/Arithmetic.kt
@@ -2,9 +2,9 @@ package pfb.basics
 
 fun main(args: Array<String>) {
     var sum = 0
-    for (i in 1..10) {
+    for (i in 0..10) {
         if(i%2 == 0) {
-            sum = sum + (i*i)
+            sum = sum + i
         }
     }
     print(sum)
