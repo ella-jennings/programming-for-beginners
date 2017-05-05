e83b7f6605a5dba4ed325c086f1cb55c7a51b2db
Ella Jennings <erj503@gmail.com>
2017-05-05 16:29:34 -0700

    Ella - Chapter 7: Challenge 35

--- src/pfb/basics/Chapter 7.kt
+++ src/pfb/basics/Chapter 7.kt
@@ -1,12 +1,23 @@
 package pfb.basics
 
 fun main(args: Array<String>) {
-    val str = "Can you understand this?"
-    var noSpaces = ""
+    val str = "Mind  the    gap"
+    var normalSpace = ""
+    var wasSpace = false
     for (c in str) {
-        if (c != ' ') {
-        noSpaces = noSpaces + c
+        if (c == ' ') {
+            if (wasSpace == true) {
+                normalSpace = normalSpace
+            }
+            else if(wasSpace == false) {
+                normalSpace = normalSpace + c
+                wasSpace = true
+            }
+
+        } else {
+            normalSpace = normalSpace + c
+            wasSpace = false
+            }
         }
-    }
-    println(noSpaces)
+    println(normalSpace)
 }
