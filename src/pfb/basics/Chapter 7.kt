6814147a3707112e53b6bbe8220b94ce1344ea19
Ella Jennings <erj503@gmail.com>
2017-05-04 23:16:51 -0700

    Ella - Chapter 7: Challenge 34

--- src/pfb/basics/Chapter 7.kt
+++ src/pfb/basics/Chapter 7.kt
@@ -1,12 +1,12 @@
 package pfb.basics
 
 fun main(args: Array<String>) {
-    val str = "How long is a piece of string?"
-    var spaceCount = 0
+    val str = "Can you understand this?"
+    var noSpaces = ""
     for (c in str) {
-        if (c == 'a' || c == 'e' || c == 'i' || c == 'o' || c == 'u') {
-            spaceCount = spaceCount + 1
+        if (c != ' ') {
+        noSpaces = noSpaces + c
         }
     }
-    println("Number of vowels: $spaceCount")
+    println(noSpaces)
 }
