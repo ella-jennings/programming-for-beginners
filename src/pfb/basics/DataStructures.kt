6e19421c5f8068e55933a4bfb8729f01dabfedfb
Ella Jennings <erj503@gmail.com>
2017-05-05 18:58:38 -0700

    Ella - Chapter 8: Challenge 39

--- src/pfb/basics/DataStructures.kt
+++ src/pfb/basics/DataStructures.kt
@@ -1,18 +1,17 @@
 package pfb.basics
 
 fun main(args: Array<String>) {
-    val sightings = mutableListOf<String>()
-    sightings.add("emu")
-    sightings.add("magpie")
-    sightings.add("galah")
-    sightings.add("emu")
+    val birds = mutableSetOf<String>()
+    birds.add("emu")
+    birds.add("magpie")
+    birds.add("galah")
+    birds.add("emu")
 
-    println("Number of bird sightings: " + sightings.size)
-    if(sightings.contains("emu")) {
+    println("Number of bird species: " + birds.size)
+    if (birds.contains("emu")) {
         println("Saw and emu!")
     }
-    if(sightings.contains("brogla")) {
+    if (birds.contains("brogla")) {
         println("Saw a brogla!")
     }
-    println("Third sighting: " + sightings[2])
 }
