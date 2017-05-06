0cc1f1836a81679457dec200d901eda008dbe6ec
Ella Jennings <erj503@gmail.com>
2017-05-05 18:44:45 -0700

    Ella - Chapter 7: Challenge 37

--- src/pfb/basics/Chapter 7.kt
+++ src/pfb/basics/Chapter 7.kt
@@ -2,5 +2,9 @@ package pfb.basics
 
 fun main(args: Array<String>) {
     val str = "back"
-    println(str)
+    var result = ""
+    for (c in str) {
+        result = c + result
+    }
+    print(result)
 }
