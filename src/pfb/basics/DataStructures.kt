2eb29c6c659763890aac243b08eb768fc78b479d
Ella Jennings <erj503@gmail.com>
2017-05-05 19:03:47 -0700

    Ella - Chapter 8: Challenge 40 (maps)

--- src/pfb/basics/DataStructures.kt
+++ src/pfb/basics/DataStructures.kt
@@ -1,17 +1,12 @@
 package pfb.basics
 
 fun main(args: Array<String>) {
-    val birds = mutableSetOf<String>()
-    birds.add("emu")
-    birds.add("magpie")
-    birds.add("galah")
-    birds.add("emu")
+    val nameToAge = mutableMapOf<String, Int>()
+    nameToAge.put("Harry", 15)
+    nameToAge.put("Luna", 16)
+    nameToAge.put("Snape", 36)
 
-    println("Number of bird species: " + birds.size)
-    if (birds.contains("emu")) {
-        println("Saw and emu!")
-    }
-    if (birds.contains("brogla")) {
-        println("Saw a brogla!")
-    }
+    println("Harry's age: " + nameToAge["Harry"])
+    println("Luna's age: " + nameToAge["Luna"])
+    println("Snape's age: " + nameToAge["Snape"])
 }
