4bd8f5d691c7dfca003b4365b71b7d290cf23438
Ella Jennings <erj503@gmail.com>
2017-05-08 21:00:18 +0100

    Ella 9:44 changed to set and made more readable

--- chapter-9/src/pfb/files/HelloWorldFile.kt
+++ chapter-9/src/pfb/files/HelloWorldFile.kt
@@ -5,31 +5,16 @@ import java.nio.file.Paths
 
 fun main(args: Array<String>) {
     val helloWorld = Paths.get("files/BirdList.txt")
-    val strings = Files.readAllLines(helloWorld)
+    val birds = Files.readAllLines(helloWorld)
 
     // create the list from the file
-    val listOfBirds = mutableListOf<String>()
-    for (string in strings) {
-        listOfBirds.add(string)
+    val listOfBirds = mutableSetOf<String>()
+    for (bird in birds) {
+        listOfBirds.add(bird)
     }
-
-    // count the species
-    val speciesToCount = mutableMapOf<String, Int>()
-    for (sighting in listOfBirds) {
-        val countSoFar = speciesToCount[sighting] ?:0
-        val updatedCount = countSoFar + 1
-        speciesToCount.put(sighting, updatedCount)
-    }
-
-    // how many per species?
-    for (species in speciesToCount.keys) {
-        val count = speciesToCount[species]
-        println("Number of $species sightings: $count")
-
-    }
-
+    
     // total species
-    val totalSpecies = speciesToCount.size
+    val totalSpecies = listOfBirds.size
     println()
     println("A total of $totalSpecies species were seen.")
 }
