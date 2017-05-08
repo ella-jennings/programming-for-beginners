237831a1f96a881336d8056c1e9290f8bcec92c2
Ella Jennings <erj503@gmail.com>
2017-05-08 20:18:27 +0100

    Ella - Chapter 9: Challenge 43

--- chapter-9/src/pfb/files/HelloWorldFile.kt
+++ chapter-9/src/pfb/files/HelloWorldFile.kt
@@ -4,7 +4,7 @@ import java.nio.file.Files
 import java.nio.file.Paths
 
 fun main(args: Array<String>) {
-    val helloWorld = Paths.get("files/HelloWorld.txt")
+    val helloWorld = Paths.get("files/HelloWorldTwice.txt")
     val strings = Files.readAllLines(helloWorld)
     println("strings = $strings")
 }
