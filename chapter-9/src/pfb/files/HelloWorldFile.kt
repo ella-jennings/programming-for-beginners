4cfcc521048fc490f1045ff2e7c22785cd00e095
Ella Jennings <erj503@gmail.com>
2017-05-08 20:49:51 +0100

    Ella - Chapter 9: Challenge 44 initial solution based on book (list and Map)

--- chapter-9/src/pfb/files/HelloWorldFile.kt
+++ chapter-9/src/pfb/files/HelloWorldFile.kt
@@ -4,7 +4,33 @@ import java.nio.file.Files
 import java.nio.file.Paths
 
 fun main(args: Array<String>) {
-    val helloWorld = Paths.get("files/HelloWorldTwice.txt")
+    val helloWorld = Paths.get("files/BirdList.txt")
     val strings = Files.readAllLines(helloWorld)
-    println("strings = $strings")
+
+    // create the list from the file
+    val listOfBirds = mutableListOf<String>()
+    for (string in strings) {
+        listOfBirds.add(string)
+    }
+
+    // count the species
+    val speciesToCount = mutableMapOf<String, Int>()
+    for (sighting in listOfBirds) {
+        val countSoFar = speciesToCount[sighting] ?:0
+        val updatedCount = countSoFar + 1
+        speciesToCount.put(sighting, updatedCount)
+    }
+
+    // how many per species?
+    for (species in speciesToCount.keys) {
+        val count = speciesToCount[species]
+        println("Number of $species sightings: $count")
+
+    }
+
+    // total species
+    val totalSpecies = speciesToCount.size
+    println()
+    println("A total of $totalSpecies species were seen.")
 }
+
