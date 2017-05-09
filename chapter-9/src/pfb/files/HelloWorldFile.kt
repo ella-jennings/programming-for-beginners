39eb41141d40a6ddd1f05e1d16b1828a85245c71
Ella Jennings <erj503@gmail.com>
2017-05-09 18:00:53 +0100

    9:45, ignore blank spaces

--- chapter-9/src/pfb/files/HelloWorldFile.kt
+++ chapter-9/src/pfb/files/HelloWorldFile.kt
@@ -4,15 +4,17 @@ import java.nio.file.Files
 import java.nio.file.Paths
 
 fun main(args: Array<String>) {
-    val helloWorld = Paths.get("files/BirdList.txt")
+    val helloWorld = Paths.get("files/BirdListWithGaps.txt")
     val birds = Files.readAllLines(helloWorld)
 
     // create the list from the file
     val listOfBirds = mutableSetOf<String>()
     for (bird in birds) {
-        listOfBirds.add(bird)
+        if (bird != "") {
+            listOfBirds.add(bird)
+        }
     }
-    
+
     // total species
     val totalSpecies = listOfBirds.size
     println()
