ca9512a8da581c0e66aa0c7d5ec349722195f029
Ella Jennings <erj503@gmail.com>
2017-05-10 17:45:58 +0100

    Ella - test that size of words returned by line is 0

--- Project-Austen/src/test/kotlin/pfb/austen/test/LineTest.kt
+++ Project-Austen/src/test/kotlin/pfb/austen/test/LineTest.kt
@@ -1,10 +1,16 @@
 package pfb.austen.test
 
 import org.junit.Test
+import org.junit.Assert
+import pfb.austen.Line
 
 class LineTest {
     @Test
-    fun test1() {
+    fun testEmpty() {
+        val line = Line("")
+        // takes two functions and compares for equality
+        // check 0 is size of words returned by line
+        Assert.assertEquals(0, line.words().size)
 
     }
 }
