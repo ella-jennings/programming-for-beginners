7281648baee4865668fff1343440025a8f413d2c
Ella Jennings <erj503@gmail.com>
2017-05-10 20:26:24 +0100

    Ella -  changed line so that one and two word tests pass, empty test fails

--- Project-Austen/src/main/kotlin/pfb/austen/Line.kt
+++ Project-Austen/src/main/kotlin/pfb/austen/Line.kt
@@ -5,7 +5,27 @@ package pfb.austen
  */
 class Line(line : String) {
 
-    fun words (): List<String> {
-        return mutableListOf()
+    val words = mutableListOf<String>()
+
+    /**
+     * initialisation block between field
+     * values and functions
+     */
+
+    init {
+        var currentWord = ""
+        for (c in line) {
+            if (c == ' ') {
+                words.add(currentWord)
+                currentWord = ""
+            } else {
+                currentWord = currentWord + c
+            }
+        }
+        words.add(currentWord)
+    }
+    fun words() : List<String> {
+        return words
     }
 }
+
