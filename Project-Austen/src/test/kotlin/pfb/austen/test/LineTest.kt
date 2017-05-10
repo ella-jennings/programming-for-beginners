208408997c79ed0ef502ff38fbd1d278462feee5
Ella Jennings <erj503@gmail.com>
2017-05-10 20:05:42 +0100

    Ella - added oneword and twoword tests which fail

--- Project-Austen/src/test/kotlin/pfb/austen/test/LineTest.kt
+++ Project-Austen/src/test/kotlin/pfb/austen/test/LineTest.kt
@@ -13,4 +13,19 @@ class LineTest {
         Assert.assertEquals(0, line.words().size)
 
     }
+
+    @Test fun oneWord() {
+        val line = Line("hello")
+        val words = line.words()
+        Assert.assertEquals(1, words.size)
+        Assert.assertEquals("hello", words[0])
+    }
+
+    @Test fun twoWords() {
+        val line = Line("hello there")
+        val words = line.words()
+        Assert.assertEquals(2, words.size)
+        Assert.assertEquals("hello", words[0])
+        Assert.assertEquals("there", words[1])
+    }
 }
