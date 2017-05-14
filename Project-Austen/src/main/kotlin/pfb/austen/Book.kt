c8cc3f887d5dd7a805c8adc0df7c6a66f8f517c9
Ella Jennings <erj503@gmail.com>
2017-05-14 21:45:38 +0100

    Ella - initial text added to book.kt

--- Project-Austen/src/main/kotlin/pfb/austen/Book.kt
+++ Project-Austen/src/main/kotlin/pfb/austen/Book.kt
@@ -1,6 +1,16 @@
 package pfb.austen
 
+import java.nio.file.Files
+import java.nio.file.Path
+import java.nio.file.Paths
+
 /**
  * Reads a book from a text file and produces word usage information.
  */
-class Book
+class Book(bookFile: Path) {
+    val histogram = Histogram()
+
+    init {
+        
+    }
+}
