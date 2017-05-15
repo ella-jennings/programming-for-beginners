dbc8fbe172245214000d732d837c9d5165914d97
Ella Jennings <erj503@gmail.com>
2017-05-15 13:21:12 +0100

    Ella - added test for horrorStory book which passes

--- Project-Austen/src/test/kotlin/pfb/austen/test/BookTest.kt
+++ Project-Austen/src/test/kotlin/pfb/austen/test/BookTest.kt
@@ -23,4 +23,27 @@ class BookTest {
         Assert.assertEquals(1, book.histogram.numberOfTimesGiven("middle"))
         Assert.assertEquals(1, book.histogram.numberOfTimesGiven("end"))
     }
+
+    @Test fun horrorStory() {
+        val book =
+                Book(Paths.get("src/test/resources/books/HorrorStory.txt"))
+        val allWords = book.histogram.allWords()
+
+        Assert.assertEquals(7, allWords.size)
+        Assert.assertTrue(allWords.contains("it"))
+        Assert.assertTrue(allWords.contains("was"))
+        Assert.assertTrue(allWords.contains("a"))
+        Assert.assertTrue(allWords.contains("dark"))
+        Assert.assertTrue(allWords.contains("and"))
+        Assert.assertTrue(allWords.contains("stormy"))
+        Assert.assertTrue(allWords.contains("night"))
+
+        Assert.assertEquals(1, book.histogram.numberOfTimesGiven("it"))
+        Assert.assertEquals(1, book.histogram.numberOfTimesGiven("was"))
+        Assert.assertEquals(1, book.histogram.numberOfTimesGiven("a"))
+        Assert.assertEquals(1, book.histogram.numberOfTimesGiven("dark"))
+        Assert.assertEquals(1, book.histogram.numberOfTimesGiven("and"))
+        Assert.assertEquals(1, book.histogram.numberOfTimesGiven("stormy"))
+        Assert.assertEquals(1, book.histogram.numberOfTimesGiven("night"))
+    }
 }
